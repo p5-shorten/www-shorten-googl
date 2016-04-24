4a63fd95d068a01412aa6903db6ee46d7c64385a
Chase Whitener <cwhitener@gmail.com>
2016-04-24 18:47:30 -0400

    version bump

--- lib/WWW/Shorten/Googl.pm
+++ lib/WWW/Shorten/Googl.pm
@@ -10,7 +10,7 @@ use URI                  ();
 
 use base qw( WWW::Shorten::generic Exporter );
 our @EXPORT  = qw( makeashorterlink makealongerlink );
-our $VERSION = '1.100';
+our $VERSION = '1.101';
 $VERSION = eval $VERSION;
 use constant API_URL => 'https://www.googleapis.com/urlshortener/v1/url';
 
